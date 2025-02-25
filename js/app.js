7de1747d2c1400dc0c959cb444c92f23d2d3b9ba
mariorogerioamorim <rooger_amorim@hotmail.com>
2025-02-24 21:46:32 -0300

    Carrinho de Compras

---
+++ js/app.js
@@ -0,0 +1,32 @@
+    let totalGeral;
+    limpar();
+
+    
+    function adicionar() {
+    //recuperar valores = nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = parseFloat(produto.split('R$')[1]);
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+     //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    //adicionar no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul"> ${preco}</span>
+        </section>`
+    //atualizar o valor total
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    quantidade = document.getElementById('quantidade').value = 0;
+
+
+}
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0';
+
+}
